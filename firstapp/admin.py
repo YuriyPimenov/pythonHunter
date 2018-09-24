1ad123152cbfb443803f2deefcf44afe60784629
YuriyPimenov <github1991>
2018-09-24 23:41:10 +0500

    Настройка модели и добавление связи 1 ко многим

--- firstapp/admin.py
+++ firstapp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from firstapp.models import PizzaShop
+from firstapp.models import Pizza
 # Register your models here.
 
 
 admin.site.register(PizzaShop)
+admin.site.register(Pizza)
